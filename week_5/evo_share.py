1dd6267cfb88446d7408696cb832ed3913ac4e95
JackFleming48 <jfleming48@my.bcit.ca>
2025-10-09 12:08:47 -0700

    week4 words.txt for hangman & week5 evo carshare oop 'lab'

--- week_5/evo_share.py
+++ week_5/evo_share.py
@@ -11,9 +11,28 @@ class Evo:
         self.driver = driver_name
         self.in_use = True
 
+    def drive(self, driven):
+        if driven < 0:
+            raise AttributeError("Driven distance cannot be 0")
+        elif self.driver == None:
+            raise  RuntimeError("There must be a driver")
+        self.distance += driven
 
-    def drive(self):
-        pass
 
     def end_rental(self):
-        pass
+        if not self.in_use:
+            raise RuntimeError("There must be a driver")
+        self.in_use = False
+        self.driver = None
+        return self.distance
+    
+def main():
+    car = Evo()
+    car.start_rental("khabib")
+    car.drive(105)
+    car.drive(15)
+    total = car.end_rental()
+    print(f"its going to cost you ${float(total)}")
+
+if __name__ == "__main__":
+    main()
