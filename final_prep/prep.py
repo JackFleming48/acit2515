43e56b1b45518018d9a30fabda96ce3e8a87e9d6
JackFleming48 <jfleming48@my.bcit.ca>
2025-12-04 14:55:02 -0800

    important changes

---
+++ final_prep/prep.py
@@ -0,0 +1,17 @@
+'''
+venv
+Instance vs. Class
+Objects, Classes, Encapsulation
+Methods and Members
+Convert a function to an attribute
+Inheritance Super() and Method overriding
+Abstract Base Classes
+Testing, Pytest, pytest fixtures
+Flask
+ORM - DB, Model, How we execute, how we make queries and statements
+    - Add and remove object and commit those changes
+Difference betwen relationships between classes and object and the foreign key constraint
+running the flask app
+App route + BP Route decorator
+Views are simply functions, how to get data in them
+'''
